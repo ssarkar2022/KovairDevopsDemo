4a69d96df3ff16c44d02109c6a62a2fa2a4c5933
prasenjitkovair <prasenjitok@gmail.com>
2021-07-07 17:05:46 +0530

    first commit

---
+++ src/main/java/com/kovair/devops/Helloabc.java
@@ -0,0 +1,16 @@
+package com.kovair.devops;
+
+public class Helloabc {
+
+	public Helloabc() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hiii");
+		System.out.println("hello new yghgjg");
+		// TODO Auto-generated method stub
+//new171 modified new12khjjkhsg
+	}
+
+}
