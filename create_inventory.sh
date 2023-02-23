a2a8a43fbdf20519896cc220246b8d78c7372580
saaj562 <sajjad@kovair.com>
2023-02-23 11:29:06 +0530

    Update create_inventory.sh

--- create_inventory.sh
+++ create_inventory.sh
@@ -1,11 +1,11 @@
-[ -f /home/kovair/employee_portal/Ansible-Deployment/inventory/devops_demo ] && echo "File exist" || echo "File does not exist" && rm -rf /home/kovair/employee_portal/Ansible-Deployment/inventory/devops_demo
+[ -f /home/kovair/employee_portal/master/Ansible-Deployment/inventory/devops_demo ] && echo "File exist" || echo "File does not exist" && rm -rf /home/kovair/employee_portal/master/Ansible-Deployment/inventory/devops_demo
 
-cd /home/kovair/employee_portal/TFCloud-AZURE-VM
+cd /home/kovair/employee_portal/master/TFCloud-AZURE-VM
 VM_Types=('AppServer')
 
 for vm_type in "${VM_Types[@]}"
 do
 echo "$vm_type"
 ip=$(terraform output $vm_type | /home/kovair/jq-linux64 -r)
-printf "[$vm_type]\n$ip ansible_become_pass='{{ sudo_pass }}'\n\n" >> /home/kovair/employee_portal/Ansible-Deployment/inventory/devops_demo
+printf "[$vm_type]\n$ip ansible_become_pass='{{ sudo_pass }}'\n\n" >> /home/kovair/employee_portal/master/Ansible-Deployment/inventory/devops_demo
 done
